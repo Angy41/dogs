fb9d95f581f0be4ba507fbd060b93a2c77025849
Angy41 <109866448+Angy41@users.noreply.github.com>
2023-08-02 15:48:12 +0200

    third

--- dogs.go
+++ dogs.go
@@ -1,9 +1,19 @@
 package dogs
 
+import "github.com/Angy41/puppy"
+
 func Bark() string {
 	return "Whoa"
 }
 
 func Barks() string {
 	return "Whoa Whoa"
+}
+
+func BigBark() string {
+	return puppy.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return puppy.WhenGrownUp(Barks())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Angy41/dogs
 
 go 1.20
+
+require github.com/Angy41/puppy v0.0.0-20230802133022-a98d48220a8a // indirect
